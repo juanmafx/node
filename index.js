cb336ca3af52ce6624f0e55d4e4ef18daa713b5f
juanmafx <jramallo1989@gmail.com>
2021-01-31 00:19:33 -0300

    Responding a json

--- index.js
+++ index.js
@@ -33,17 +33,48 @@ const server = http.createServer((req, res) => {
 
     req.on('end', () => {
         buffer += decoder.end();
-        // Send response
-        res.end('HW');
 
-        console.log(
-            '***\nRequested: ', trimedPath,
-            '\nWhith this method: ', method,
-            '\nQuery string parameters ', qso,
-            '\nHeaders: ', headers,
-            '\nPayload: ', buffer,
-            '\n***');
+        //Chose the handler this request should go. If not execute not found handler
+        var choseHandler = typeof (router[trimedPath]) !== 'undefined' ? router[trimedPath] : handlers.notFound;
+
+        //Construct the data object to sended to the handler
+        var data = {
+            'trimedPath': trimedPath,
+            'method': method,
+            'qso': qso,
+            'headers': headers,
+            'payload': buffer,
+        }
+
+        //Routh the reques specified on the router
+        choseHandler(data, function (statusCode, payload) {
+            //Use the status code callback by the handler, or default 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            //Use the payload called back by the handler, or default empty object
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            //Convert the payload in to a string
+            var payloadString = JSON.stringify(payload);
+
+            //Return the response
+            res.setHeader('Content-Type','aplication/json')
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            console.log(
+                '***\nRequested: ', trimedPath,
+                '\nWhith this method: ', method,
+                '\nQuery string parameters ', qso,
+                '\nHeaders: ', headers,
+                '\nPayload: ', buffer,
+                '\nSending back : ', payloadString,
+                '\n***');
+        })
+
     })
+
+
 
 
 });
@@ -51,3 +82,24 @@ const server = http.createServer((req, res) => {
 server.listen(4000, () => {
     console.log('Server listening on 4000')
 })
+
+
+// Define the handlers
+var handlers = {};
+
+// Sample handler
+handlers.sample = function (data, callback) {
+    //Callback http status code, and pyload object
+    callback(406, { 'name': 'sample hanldres' })
+};
+
+// Not Found handler
+handlers.notFound = function (data, callback) {
+    callback(404)
+
+};
+
+// Define a request router
+var router = {
+    'sample': handlers.sample
+}
